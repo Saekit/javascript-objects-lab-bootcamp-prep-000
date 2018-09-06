6267d1871975a1a40ee6edb0f7bc76d204905efc
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-06 17:45:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,5 +10,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({key}, object);
+  delete newObj.key;
   return newObj;
+}
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object.key;
+  return object;
 }
