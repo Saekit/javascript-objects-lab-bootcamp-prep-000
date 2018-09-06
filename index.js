a4bec8a64c9b9e9e6ad5eb5b1dde4efbc7e3f673
Raquel Randall <raquel.sae.randall@gmail.com>
2018-09-06 17:47:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,4 +17,4 @@ function destructivelyDeleteFromObjectByKey(object, key) {
   delete object.key;
   return object;
 }
-console.log(updateObjectWithKeyAndValue('recipes', 'apple pie', 'apples'))
+console.log(updateObjectWithKeyAndValue(recipes, 'apple pie', 'apples'))
